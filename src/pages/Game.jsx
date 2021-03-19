b949eaca03b292b1c1a0e3295af347f4cf5c54b6
andrigoBS <andrigo2000@gmail.com>
2021-03-19 18:22:54 -0300

    Open unity: deep link

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Subheading } from 'react-native-paper';
+import * as Linking from 'expo-linking';
+// import { WebView } from 'react-native-webview';
 
 const Game = () => {
-
+  let url = 'unitydl://gymkhanar';
+  Linking.openURL(url);
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Subheading>
             Carregando o Jogo...
         </Subheading>
+        {/*<WebView source={{ uri: 'https://expo.io' }} style={{ marginTop: 20 }}/>*/}
     </View>
   );
 }
